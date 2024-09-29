cf35cccacbfd1054f7db5534612f044eab64fee3
Konnichiwaitsnahoko <154089390+Konnichiwaitsnahoko@users.noreply.github.com>
2024-09-29 12:20:27 -0700

    Added footer

--- src/index.js
+++ src/index.js
@@ -11,6 +11,15 @@ root.render(
     <div>
       <h1>Weather App</h1>
       <SearchEngine />
+      <p>
+        This project was coded by{" "}
+        <a href="https://github.com/Konnichiwaitsnahoko">Nahoko Park </a> and is
+        open-sourced on{" "}
+        <a href="https://github.com/Konnichiwaitsnahoko/weather-react">
+          GitHub
+        </a>{" "}
+        and hosted on Netlify.
+      </p>
     </div>
   </React.StrictMode>
 );
